0e9f4d4ddf030487fc3f375d277b86df42a63a2a
l46kok <l46kok@localhost>
2016-08-22 10:53:38 -0600

    Fate FTP Replay Sender Init Commit

---
+++ Fate.FTPReplaySender/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Fate.FTPReplaySender.Utility;
+using NLog;
+
+namespace Fate.FTPReplaySender
+{
+    internal static class Program
+    {
+        private const int CONSOLE_CLEAR_DISPLAY_LIMIT = 50; //Clears console every nth time parsing has run
+        private const string DEFAULT_CONFIG_FILE_PATH = "config.cfg";
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            logger.Trace("Starting FTP Replay Sender");
+            ConfigHandler configHandler = new ConfigHandler(DEFAULT_CONFIG_FILE_PATH);
+            if (!configHandler.IsConfigFileValid())
+                logger.Trace("Error loading default config file: {0}", DEFAULT_CONFIG_FILE_PATH);
+            else
+            {
+                logger.Trace("Loading default config file: {0}", DEFAULT_CONFIG_FILE_PATH);
+                configHandler.LoadConfig();
+            }
+        }
+    }
+}
