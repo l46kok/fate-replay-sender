ebec4c0f625fc0a92bbb0f550d7377bcdc8850be
l46kok <2355949+l46kok@users.noreply.github.com>
2016-08-22 10:53:38 -0600

    Fate FTP Replay Sender Init Commit

---
+++ Fate.FTPReplaySender/Utility/ConfigHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Fate.FTPReplaySender.Utility
+{
+    public class ConfigHandler
+    {
+        public string ReplayPath { get; private set; }
+        public string FTPAddr { get; set; }
+        public string FTPUserName { get; set; }
+        public string FTPPassword { get; set; }
+
+        private string _configFilePath;
+        private readonly List<string> _fileContent = new List<string>();
+        public ConfigHandler(string configFilePath)
+        {
+            _configFilePath = configFilePath;
+            if (File.Exists(configFilePath))
+            {
+                _fileContent = new List<string>(File.ReadAllLines(configFilePath));
+            }
+        }
+
+        public bool IsConfigFileValid()
+        {
+            return _fileContent.Any();
+        }
+
+        public void LoadConfig()
+        {
+            ReplayPath = GetConfigString("replaypath");
+            FTPAddr = GetConfigString("ftpaddr");
+            ReplayPath = GetConfigString("ftpusername");
+            ReplayPath = GetConfigString("ftppassword");
+        }
+
+        private string GetConfigString(string key)
+        {
+            foreach (string line in _fileContent)
+            {
+                if (String.IsNullOrEmpty(line))
+                    continue;
+                if (line[0] == '#') //Config Comment
+                    continue;
+                string[] configValueArray = line.Split('=');
+                if (configValueArray.Length != 2)
+                    continue;
+                if (configValueArray[0] == key)
+                    return configValueArray[1];
+            }
+            return String.Empty;
+        }
+    }
+}
